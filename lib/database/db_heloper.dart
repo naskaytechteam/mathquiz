a9ca613ec6f099b48f4b43e9adb8fb356863745f
nikirana22 <nikirana1983@gmail.com>
2022-09-22 14:46:14 +0530

    feat(db_helper): implement database class

---
+++ lib/database/db_heloper.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DbHelper {
+  static Database? _database;
+
+  Future<Database?> get database async{
+    _database ??= await _getDatabase();
+    return _database;
+  }
+
+  Future<Database> _getDatabase() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'sqlite.db');
+    File file = File(path);
+    bool isFileExist = await file.exists();
+    if (!isFileExist) {
+      await file.create();
+    }
+    ByteData assetBundle = await rootBundle.load(join('assets', 'sqlite.db'));
+    Uint8List dataList = assetBundle.buffer
+        .asUint8List(assetBundle.offsetInBytes, assetBundle.lengthInBytes);
+    await file.writeAsBytes(dataList);
+    return await openDatabase(path,version: 1);
+  }
+}
