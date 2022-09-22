07626796f2768502d1b909132e9ede96e4b60912
nikirana22 <nikirana1983@gmail.com>
2022-09-22 18:17:17 +0530

    refector(quiz_provider): refecture ques temp and options

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mathquiz/model/quiz_data.dart';
+import 'package:mathquiz/provider/quiz_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<QuizProvider>.value(value: QuizProvider())
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,13 +21,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home:const MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -27,8 +36,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    QuizProvider quizProvider = Provider.of<QuizProvider>(context);
+    List<QuizData> list = quizProvider.quizData;
+
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+        margin: const EdgeInsets.only(top: 60),
+        child: list.isNotEmpty
+            ? Column(
+                children: [
+                  Text(
+                    list[_index].ques,
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                  Text(
+                    list[_index].options[0].toString(),
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                  Text(
+                    list[_index].options[1].toString(),
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                  Text(
+                    list[_index].options[2].toString(),
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                  Text(
+                    list[_index].options[3].toString(),
+                    style: const TextStyle(fontSize: 40),
+                  )
+                ],
+              )
+            : const CircularProgressIndicator(),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(onPressed: (){
+
+          }),
+          FloatingActionButton(onPressed: (){
+            print('length -> ${list.length} index -> $_index');
+            if(_index<list.length-1){
+              setState(() {
+                _index++;
+              });
+            }
+          })
+        ],
+      ),
+    );
   }
 }
--- lib/provider/quiz_provider.dart
+++ lib/provider/quiz_provider.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/foundation.dart';
+import 'package:function_tree/function_tree.dart';
 import 'package:mathquiz/database/db_heloper.dart';
 import '/model/quiz_data.dart';
+import 'dart:math';
 
-class QuizProvider extends ChangeNotifier{
-   List<QuizData> _quizData=[];
-  QuizProvider(){
+class QuizProvider extends ChangeNotifier {
+  List<QuizData> _quizData = [];
+  final Random _random = Random();
+
+  QuizProvider() {
     getData();
   }
-  void getData()async{
-    DbHelper helper=DbHelper();
-    _quizData=await helper.readData();
+
+  void getData() async {
+    DbHelper helper = DbHelper();
+    _quizData = await helper.readData();
+
+    for (var element in _quizData) {
+      _refectorQuesTemp(element);
+    }
   }
 
-  List<QuizData> get quizData=>[..._quizData];
+  void _refectorQuesTemp(QuizData data) {
+    for (int a = 1; a <= data.totalValue; a++) {
+      int value = _random.nextInt(100);
+      data.values.add(value);
+      data.ques=data.ques.replaceAll('V$a', value.toString());
+    }
+    _addOptions(data);
+    notifyListeners();
+  }
+  void _addOptions(QuizData data){
+    for(int a=1;a<=data.totalValue;a++){
+      data.formula=data.formula.replaceAll('V$a',data.values[a-1].toString() );
+    }
+    int ans=data.formula.interpret().toInt();
+    _makeOptions(data, ans);
+  }
+  void _makeOptions(QuizData data,int answer){
+    data.options.add(answer);
+    data.options.add(answer+5);
+    data.options.add(answer-5);
+    data.options.add(answer-10);
 
+  }
+
+
+  List<QuizData> get quizData => [..._quizData];
 }
