6e81f3a9419873f7d34743c62cb9b8b1e72e2dec
nikirana22 <nikirana1983@gmail.com>
2022-09-28 11:15:58 +0530

    feat(option_button): add optionSelected field for button click

--- lib/widgets/option_button.dart
+++ lib/widgets/option_button.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 class OptionButton extends StatelessWidget {
   final String optionType;
-  final String option;
+  final int option;
+  final Function optionSelected;
 
-  const OptionButton({required this.optionType, required this.option,
-    Key? key}) : super(key: key);
+  const OptionButton(
+      {required this.optionType,
+      required this.optionSelected,
+      required this.option,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,29 +18,31 @@ class OptionButton extends StatelessWidget {
     double height = size.height;
     double width = size.width;
 
-    return TextButton.icon(
-        onPressed: () {},
-        icon: Container(
-          height: height * 0.04,
-          width: width * 0.08,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-              color: Colors.grey.shade200.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(19)),
-          child: Text(
-            optionType,
-            style: const TextStyle(color: Colors.black),
+    return Align(
+      child: TextButton.icon(
+          onPressed: () => optionSelected(option),
+          icon: Container(
+            height: height * 0.04,
+            width: width * 0.08,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade200.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(19)),
+            child: Text(
+              optionType,
+              style: const TextStyle(color: Colors.black),
+            ),
           ),
-        ),
-        style: TextButton.styleFrom(
-            minimumSize: Size(width * 0.35, height * 0.03),
-            side: const BorderSide(width: 0.1, color: Colors.black),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20))),
-        label: Text(
-          option,
-          style:const TextStyle(
-              color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),
-        ));
+          style: TextButton.styleFrom(
+              minimumSize: Size(width * 0.35, height * 0.03),
+              side: const BorderSide(width: 0.5, color: Colors.black),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20))),
+          label: Text(
+            option.toString(),
+            style: const TextStyle(
+                color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),
+          )),
+    );
   }
 }
