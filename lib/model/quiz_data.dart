3a4b98111986bc5de68446b13297003a66f34212
nikirana22 <nikirana1983@gmail.com>
2022-09-28 11:11:50 +0530

    refactor (quiz_data): change answer type and option list type

--- lib/model/quiz_data.dart
+++ lib/model/quiz_data.dart
@@ -2,13 +2,13 @@ class QuizData {
   String ques;
   int totalValue;
   String formula;
-  List<num> options = [];
+  List<int> options = [];
   List<int> values = [];
-  String? answer;
+  int? answer;
 
   QuizData(this.ques, this.formula, this.totalValue);
 
-  factory QuizData.fromMap(Map<String, Object> map) {
+  factory QuizData.fromMap(Map<String, Object?> map) {
     return QuizData(
         map['ques'] as String, map['formula'] as String, map['value']as int);
   }
