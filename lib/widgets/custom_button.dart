8482dd5c4278860139d9c43e50561f91ecc2df5a
nikirana22 <nikirana1983@gmail.com>
2022-09-29 09:33:27 +0530

    refactor (custom_button): Change TextButton widget from Container

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -18,31 +18,45 @@ class CustomButton extends StatelessWidget {
     double height = size.height;
     double width = size.width;
 
-    return Align(
-      child: TextButton.icon(
-          onPressed: () => optionSelected(option),
-          icon: Container(
-            height: height * 0.04,
-            width: width * 0.08,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.grey.shade200.withOpacity(0.8),
-                borderRadius: BorderRadius.circular(19)),
-            child: Text(
-              optionType,
-              style: const TextStyle(color: Colors.black),
-            ),
+    return InkWell(
+      onTap: ()=>optionSelected(option),
+      child: Align(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+          height: height * 0.06,
+          width: width * 0.35,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(color: Colors.grey.shade200, width: 2)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: height * 0.04,
+                width: width * 0.08,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade200.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(19)),
+                child: Text(
+                  optionType,
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: width * 0.2,
+                height: height * 0.05,
+                child: Text(
+                  option.toString(),
+                  style:const TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
           ),
-          style: TextButton.styleFrom(
-              minimumSize: Size(width * 0.35, height * 0.03),
-              side: const BorderSide(width: 0.5, color: Colors.black),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20))),
-          label: Text(
-            option.toString(),
-            style: const TextStyle(
-                color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),
-          )),
+        ),
+      ),
     );
   }
 }
