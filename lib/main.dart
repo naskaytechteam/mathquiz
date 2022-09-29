176f61683c267aa6fc4213cb410e5ca64e6f572f
nikirana22 <nikirana1983@gmail.com>
2022-09-29 09:04:05 +0530

    refector(main): remove StateFulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mathquiz/model/quiz_data.dart';
-import 'package:mathquiz/provider/quiz_provider.dart';
+import '/provider/quiz_provider.dart';
+import '/screens/quiz_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -23,72 +23,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _index = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    QuizProvider quizProvider = Provider.of<QuizProvider>(context);
-    List<QuizData> list = quizProvider.quizData;
-
-    return Scaffold(
-      body: Container(
-        color: Colors.red,
-        margin: const EdgeInsets.only(top: 60),
-        child: list.isNotEmpty
-            ? Column(
-                children: [
-                  Text(
-                    list[_index].ques,
-                    style: const TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    list[_index].options[0].toString(),
-                    style: const TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    list[_index].options[1].toString(),
-                    style: const TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    list[_index].options[2].toString(),
-                    style: const TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    list[_index].options[3].toString(),
-                    style: const TextStyle(fontSize: 40),
-                  )
-                ],
-              )
-            : const CircularProgressIndicator(),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          FloatingActionButton(onPressed: (){
-
-          }),
-          FloatingActionButton(onPressed: (){
-            print('length -> ${list.length} index -> $_index');
-            if(_index<list.length-1){
-              setState(() {
-                _index++;
-              });
-            }
-          })
-        ],
+      home: const Scaffold(
+        body: QuizScreen(),
       ),
     );
   }
