48f088566350c9a5f44a701d9fb4462375dff4e4
nikirana22 <nikirana1983@gmail.com>
2022-09-29 09:43:48 +0530

    feat (quiz_data): add quesType field

--- lib/parsers/quiz_data_parser.dart
+++ lib/parsers/quiz_data_parser.dart
@@ -1,12 +1,12 @@
 import '../model/quiz_data.dart';
 
 class QuizDataParser {
-  static QuizData _data(Map<String, Object?>map) {
-    return QuizData(
-        map['ques'] as String, map['formula'] as String, map['value'] as int);
+  static QuizData _data(Map<String, Object?> map) {
+    return QuizData(map['ques'] as String, map['formula'] as String,
+        map['totalValues'] as int, map['quesType'] as String);
   }
 
-  static List<QuizData> quizDataList(List<Map<String, Object?>>l) {
-    return  l.map((e) =>_data(e)).toList();
+  static List<QuizData> quizDataList(List<Map<String, Object?>> l) {
+    return l.map((e) => _data(e)).toList();
   }
 }
