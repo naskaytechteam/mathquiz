3295721c5c3c993937f33691e1410e2d079bf136
nikirana22 <nikirana1983@gmail.com>
2022-09-29 09:43:19 +0530

    feat (quiz_data): add quesType field

--- lib/model/quiz_data.dart
+++ lib/model/quiz_data.dart
@@ -1,16 +1,16 @@
 class QuizData {
   String ques;
-  int totalValue;
+  int totalValues;
   String formula;
   List<int> options = [];
   List<int> values = [];
   int? answer;
+  String quesType;
 
-  QuizData(this.ques, this.formula, this.totalValue);
+  QuizData(this.ques, this.formula, this.totalValues, this.quesType);
 
   factory QuizData.fromMap(Map<String, Object?> map) {
-    return QuizData(
-        map['ques'] as String, map['formula'] as String, map['value']as int);
+    return QuizData(map['ques'] as String, map['formula'] as String,
+        map['value'] as int, map['quesType'] as String);
   }
-
 }
