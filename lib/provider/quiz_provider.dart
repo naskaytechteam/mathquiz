e28fd10d36ea4347b6078a167a053f5aed953e5c
nikirana22 <nikirana1983@gmail.com>
2022-09-30 16:25:36 +0530

    refactor(quiz_provider): change ques get logic from database

--- lib/provider/quiz_provider.dart
+++ lib/provider/quiz_provider.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/foundation.dart';
 import 'package:function_tree/function_tree.dart';
-import '/database/db_heloper.dart';
-import '/model/quiz_data.dart';
+import 'package:mathquiz/model/util.dart';
+import '/database/db_helper.dart';
+import '/model/ques_tenp.dart';
 import 'dart:math';
 
+enum QUESTYPE {
+  lcm,
+  hcf,
+  simple,
+  wordProblem,
+  ratio,
+  friction,
+}
+
 class QuizProvider extends ChangeNotifier {
-  List<QuizData> _quizData = [];
+  List<QuesTemp> _quizData = [];
   final Random _random = Random();
 
   QuizProvider() {
+
     _getData();
   }
 
-  List<QuizData> get quizData => [..._quizData];
+  List<QuesTemp> get quizData => [..._quizData];
 
   void _getData() async {
     DbHelper helper = DbHelper();
@@ -20,59 +31,81 @@ class QuizProvider extends ChangeNotifier {
     _refactorQuesTemp(_quizData);
   }
 
-  void _refactorQuesTemp(List<QuizData> list) {
+  void _hcfQuestion(QuesTemp data) {
+    for (int a = 0; a < data.valuePlaceholders; a++) {
+      int value = _random.nextInt(100);
+      data.randomValues.add(value);
+      data.ques = data.ques.replaceAll('V${a + 1}', value.toString());
+    }
+    _hcf(data);
+  }
+
+  void _hcf(QuesTemp data) {
+        int hcf=Util.hcf(data.randomValues[0],data.randomValues[1]);
+        String formula=data.formula.replaceAll('hcf', hcf.toString());
+        int answer=formula.interpret().toInt();
+      _makeOptions(data, answer);
+  }
+
+  void _refactorQuesTemp(List<QuesTemp> list) {
     for (int arr = 0; arr < list.length; arr++) {
-      QuizData data = list[arr];
-      if (_isNameContain(data.ques)) {
-        bool isValuePositive = _nameQuestion(data);
-        if (!isValuePositive) {
-          bool a = list.remove(data);
-          if (a) {
-            arr--;
-          }
+      QuesTemp quesTemp = list[arr];
+      if (QUESTYPE.hcf.index == quesTemp.quesType) {
+        _hcfQuestion(quesTemp);
+        continue;
+      }
+      else if(QUESTYPE.lcm.index==quesTemp.quesType) {
+
+      }
+      else if(QUESTYPE.simple.index==quesTemp.quesType) {
+        _simpleQuestion(quesTemp);
+      }
+      else if(QUESTYPE.wordProblem.index==quesTemp.quesType) {
+        bool isAnswerPositive=_wordProblemQues(quesTemp);
+        if(!isAnswerPositive) {
+          _quizData.remove(quesTemp);
+          arr--;
           continue;
         }
-      } else {
-        _simpleQuestion(data);
       }
-      _addOptions(data);
+      _addOptions(quesTemp);
     }
   }
 
-  void _simpleQuestion(QuizData data) {
-    for (int sec = 0; sec < data.totalValue; sec++) {
+  void _simpleQuestion(QuesTemp data) {
+    for (int sec = 0; sec < data.valuePlaceholders; sec++) {
       int randomValue = _random.nextInt(100);
-      data.values.add(randomValue);
-      data.ques =
-          data.ques.replaceAll('V${sec + 1}', data.values[sec].toString());
+      data.randomValues.add(randomValue);
+      data.ques = data.ques
+          .replaceAll('V${sec + 1}', data.randomValues[sec].toString());
     }
   }
 
-  bool _isValueNegative(QuizData data) {
+  bool _isValueNegative(QuesTemp data) {
     String formula = data.formula;
     String ques = data.ques;
-    for (int sec = 0; sec < data.totalValue; sec++) {
+    for (int sec = 0; sec < data.valuePlaceholders; sec++) {
       int randomValue = _random.nextInt(100);
-      data.values.add(randomValue);
-      ques = ques.replaceAll('V${sec + 1}', data.values[sec].toString());
-      formula = formula.replaceAll('V${sec + 1}', data.values[sec].toString());
+      data.randomValues.add(randomValue);
+      ques = ques.replaceAll('V${sec + 1}', data.randomValues[sec].toString());
+      formula =
+          formula.replaceAll('V${sec + 1}', data.randomValues[sec].toString());
     }
     num answer = formula.interpret();
     bool checkAnswer = answer > 0;
     if (checkAnswer) {
       data.ques = ques;
     }
-
     return checkAnswer;
   }
 
-  bool _nameQuestion(QuizData data) {
+  bool _wordProblemQues(QuesTemp data) {
     bool checkAnswer = _isValueNegative(data);
     if (checkAnswer) {
       return true;
     } else {
       for (int a = 0; a < 5; a++) {
-        data.values = []; //
+        data.randomValues = []; //
         checkAnswer = _isValueNegative(data);
         if (checkAnswer) {
           break;
@@ -82,17 +115,16 @@ class QuizProvider extends ChangeNotifier {
     }
   }
 
-  void _addOptions(QuizData data) {
-    for (int a = 1; a <= data.totalValue; a++) {
+  void _addOptions(QuesTemp data) {
+    for (int a = 1; a <= data.valuePlaceholders; a++) {
       data.formula =
-          data.formula.replaceAll('V$a', (data.values[a - 1]).toString());
+          data.formula.replaceAll('V$a', (data.randomValues[a - 1]).toString());
     }
-
     int ans = data.formula.interpret().toInt();
     _makeOptions(data, ans);
   }
 
-  void _makeOptions(QuizData data, int answer) {
+  void _makeOptions(QuesTemp data, int answer) {
     data.answer = answer;
     data.options.add(answer);
     data.options.add(answer + 5);
@@ -101,7 +133,7 @@ class QuizProvider extends ChangeNotifier {
     _swipeOption(data);
   }
 
-  void _swipeOption(QuizData data) {
+  void _swipeOption(QuesTemp data) {
     for (int a = 0; a < 2; a++) {
       int firstRandomValue = _random.nextInt(4);
       int getFirst = data.options[a];
@@ -111,17 +143,4 @@ class QuizProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  bool _isNameContain(String name) {
-    List<String> list = [
-      ' has ',
-      ' she ',
-      ' he ',
-      ' have ',
-      ' his ',
-      ' are ',
-      ' her '
-    ];
-    Iterable iterable = list.where((element) => name.contains(element));
-    return iterable.isNotEmpty;
-  }
 }
