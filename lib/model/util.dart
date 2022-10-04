106a5534e753734ecc69127e23d9f29c7a0535f7
nikirana22 <nikirana1983@gmail.com>
2022-10-04 16:09:58 +0530

    feat (util): add formula method

--- lib/model/util.dart
+++ lib/model/util.dart
@@ -1,8 +1,23 @@
-class Util{
-  static int hcf(int firstValue,int secondValue){
+class Util {
+  static int hcf(int firstValue, int secondValue) {
     if (firstValue == 0) {
       return secondValue;
     }
     return hcf(secondValue % firstValue, firstValue);
+  }
+
+  static String ratio(int first,int second) {
+    for(int ar=2;ar<=first &&ar<=second;ar++){
+      while (first%ar==0&&second%ar==0){
+        first~/=ar;
+        second~/=ar;
+      }
+    }
+    return '$first : $second';
+  }
+
+  static int lcm(int a, int b)
+  {
+    return (a ~/ hcf(a, b)) * b;
   }
 }
