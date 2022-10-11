f30b6a313ae370e4c95a7d670d020c2f02a6cb2b
nikirana22 <nikirana1983@gmail.com>
2022-10-11 20:16:29 +0530

    feat(ques_score_details): add ques_score_details widget

---
+++ lib/widgets/ques_score_details.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class QuesScoreDetails extends StatelessWidget {
+  final String firstResult;
+  final String secondResult;
+  final String firstType;
+  final String secondType;
+  final Color? firstTextColor;
+  final Color? secondTextColor;
+
+  const QuesScoreDetails(
+      {required this.firstType,
+      required this.secondType,
+      this.firstTextColor,
+      this.secondTextColor,
+      required this.firstResult,
+      required this.secondResult,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double height = size.height;
+    double width = size.width;
+    return SizedBox(
+      height: height * 0.1,
+      width: width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          buildQuestionScoreDetails(firstResult, firstType, height, width,
+              textColor: firstTextColor),
+          buildQuestionScoreDetails(secondResult, secondType, height, width,
+              textColor: secondTextColor)
+        ],
+      ),
+    );
+  }
+
+  Widget buildQuestionScoreDetails(
+      String firstDetails, String type, double height, double width,
+      {Color? textColor = const Color.fromRGBO(56, 61, 110, 1)}) {
+    return Container(
+      height: height * 0.1,
+      width: width * 0.3,
+      alignment: Alignment.topCenter,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(firstDetails,
+              style: TextStyle(
+                  color: textColor, fontSize: 20, fontWeight: FontWeight.bold)),
+          Text(type)
+        ],
+      ),
+    );
+  }
+}
