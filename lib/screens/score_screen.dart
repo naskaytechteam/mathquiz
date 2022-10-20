a8a84f638c4cd3082a8994d2a4143f759bcc24fc
nikirana22 <nikirana1983@gmail.com>
2022-10-20 08:17:08 +0530

    feat(score_screen): add generatePdf feature

--- lib/screens/score_screen.dart
+++ lib/screens/score_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:mathquiz/provider/quiz_provider.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pdf;
+import 'package:provider/provider.dart';
+import '/screens/review_answer.dart';
 import '../widgets/scorescreenwidgets/score_details.dart';
 import '../widgets/scorescreenwidgets/your_point.dart';
 
@@ -32,7 +39,7 @@ class ScoreScreen extends StatelessWidget {
                 )),
             Positioned(
               bottom: 0,
-              child: _buildBottomWidget(height, width),
+              child: _buildBottomWidget(height, width, context),
             ),
             const Positioned(top: 50, left: 0, right: 0, child: YourPoint()),
             Positioned(
@@ -61,7 +68,8 @@ class ScoreScreen extends StatelessWidget {
   }
 
 //refactor Name
-  Widget _buildSixWidget(double height, double width, int index) {
+  Widget _buildSixWidget(
+      double height, double width, int index, BuildContext context) {
     List<IconData> iconList = [
       Icons.refresh,
       Icons.remove_red_eye,
@@ -69,14 +77,6 @@ class ScoreScreen extends StatelessWidget {
       Icons.picture_as_pdf,
       Icons.home,
       Icons.star
-    ];
-    List<Color> colorsList = [
-      const Color.fromRGBO(83, 145, 179, 1),
-      const Color.fromRGBO(192, 141, 110, 1),
-      const Color.fromRGBO(117, 94, 211, 1),
-      Colors.blue,
-      const Color.fromRGBO(190, 112, 159, 1),
-      Colors.black38
     ];
     List<String> textList = [
       'Play Again',
@@ -86,26 +86,39 @@ class ScoreScreen extends StatelessWidget {
       'Home',
       'Leaderboard'
     ];
-    return SizedBox(
-      height: height * 0.12,
-      width: width * 0.3,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          CircleAvatar(
-            backgroundColor: colorsList[index],
-            radius: 30,
-            child: Icon(iconList[index], color: Colors.white),
-          ),
-          Text(
-            textList[index],
-          )
-        ],
+    return InkWell(
+      onTap: () {
+        if (index == 1) {
+          Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+            return const ReviewAnswer();
+          }));
+        } else if (index == 0) {
+          Navigator.pop(context);
+        } else if (index == 3) {
+          _generatePdf(context, height, width);
+        }
+      },
+      child: SizedBox(
+        height: height * 0.12,
+        width: width * 0.3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.primaries[index],
+              radius: 30,
+              child: Icon(iconList[index], color: Colors.white),
+            ),
+            Text(
+              textList[index],
+            )
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildBottomWidget(double height, double width) {
+  Widget _buildBottomWidget(double height, double width, BuildContext context) {
     return SizedBox(
       height: height * 0.4,
       width: width,
@@ -116,10 +129,80 @@ class ScoreScreen extends StatelessWidget {
               crossAxisCount: 3, crossAxisSpacing: 0),
           itemBuilder: (_, index) {
             return Align(
-              child: _buildSixWidget(height, width, index),
+              child: _buildSixWidget(height, width, index, context),
             );
           }),
     );
   }
+
+  void addAllWidgets(
+      BuildContext context, double height, List<pdf.Widget> list) {
+    QuizProvider quizProvider =
+        Provider.of<QuizProvider>(context, listen: false);
+    for (int a = 0; a < quizProvider.quesTemplateList.length; a++) {
+      list.add(pdf.RichText(
+        text: pdf.TextSpan(text: 'Ques ${a + 1} ', children: [
+          pdf.TextSpan(text: '${quizProvider.quesTemplateList[a].ques}\n')
+        ]),
+      ));
+
+      list.add(pdf.RichText(
+        text: pdf.TextSpan(
+            text: 'Ans :-> ',
+            style: const pdf.TextStyle(color: PdfColors.green),
+            children: [
+              pdf.TextSpan(
+                  text: quizProvider.quesTemplateList[a].answer.toString(),
+                  style: const pdf.TextStyle(color: PdfColors.black))
+            ]),
+      ));
+      list.add(pdf.SizedBox(height: height * 0.02));
+    }
+  }
+
+  void _generatePdf(BuildContext context, double height, double width) {
+    List<pdf.Widget> list = [];
+    addAllWidgets(context, height, list);
+    pdf.Document document = pdf.Document();
+    document.addPage(pdf.MultiPage(build: (_) {
+      return list;
+    }));
+    saveFile(document, context);
+  }
+
+  void saveFile(pdf.Document document, BuildContext context) async {
+    Directory? directory = await getExternalStorageDirectory();
+    File file = File('${directory!.path}/QuizQuestions.pdf');
+    file.writeAsBytes(await document.save())
+        .then((value) => _showSnackBar(context));
+  }
+
+  void _showSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('pdf Generated Successfully')));
+  }
+
+  pdf.Widget text(String ques, int index, num answer) {
+    return pdf.RichText(
+        text: pdf.TextSpan(
+            text: 'Ques ${index + 1} :-> ',
+            style: const pdf.TextStyle(
+              fontSize: 25,
+            ),
+            children: [
+          pdf.TextSpan(
+              text: '$ques \n',
+              style: const pdf.TextStyle(
+                fontSize: 20,
+              )),
+          pdf.TextSpan(
+              text: 'Ans :-> ',
+              style: const pdf.TextStyle(color: PdfColors.green),
+              children: [
+                pdf.TextSpan(
+                    text: answer.toString(),
+                    style: const pdf.TextStyle(color: PdfColors.black))
+              ])
+        ]));
+  }
 }
-//·
