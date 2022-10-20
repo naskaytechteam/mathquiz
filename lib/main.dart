d2166136fad0678e6e827c654a328da616a35bd1
nikirana22 <nikirana1983@gmail.com>
2022-10-20 15:44:04 +0530

    feat(ques_type_name): add font family

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme:const TextTheme(
+          headline1: TextStyle(fontFamily: 'Inconsolata')
+        )
       ),
+
       home: const Scaffold(
         body: QuesTypeScreen(),
       ),
--- lib/widgets/questypesscreenwidgets/ques_type_name.dart
+++ lib/widgets/questypesscreenwidgets/ques_type_name.dart
@@ -17,26 +17,27 @@ class QuesTypeName extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     double height = size.height;
     double width = size.width;
-    return Container(
+    return SizedBox(
       width: width,
-      color: const Color.fromRGBO(33, 0, 2, 1),
+      // color: const Color.fromRGBO(33, 0, 2, 1),
       child: InkWell(
         onTap: () => onQuesTypeSelected(questype),
         child: Container(
           width: width * 0.8,
           height: height * 0.13,
           margin: EdgeInsets.symmetric(
-              horizontal: width * 0.1, vertical: height * 0.02),
+              horizontal: width * 0.06, vertical: height * 0.02),
           decoration: BoxDecoration(
-              color: const Color.fromRGBO(45, 10, 13, 1),
+              // color: const Color.fromRGBO(45, 10, 13, 1),
+              color: const Color.fromRGBO(63, 159, 254, 1),
               borderRadius: BorderRadius.circular(20)),
           child: Row(
             children: [
               Container(
                 width: width * 0.25,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/simple.png'))),
+                // decoration: const BoxDecoration(
+                //     image: DecorationImage(
+                //         image: AssetImage('assets/simple.png'))),
               ),
               SizedBox(
                 width: width * 0.05,
@@ -52,8 +53,13 @@ class QuesTypeName extends StatelessWidget {
                       width: width,
                       child: AutoSizeText(
                         questype.name,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 25),
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontFamily: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .fontFamily),
                       ),
                     ),
                     SizedBox(
@@ -64,10 +70,10 @@ class QuesTypeName extends StatelessWidget {
                     SizedBox(
                       height: height * 0.02,
                     ),
-                    const LinearProgressIndicator(
+                    LinearProgressIndicator(
                       value: 1,
-                      backgroundColor: Colors.red,
-                      color: Color.fromRGBO(54, 58, 102, 1),
+                      backgroundColor: Colors.grey,
+                      color: Colors.white60,
                     )
                   ],
                 ),
