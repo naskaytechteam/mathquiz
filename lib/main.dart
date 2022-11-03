827db3fbd20782842591710f7622ece4eeb0cf8c
nikirana22 <nikirana1983@gmail.com>
2022-11-03 18:52:03 +0530

    refactor(main): remove MultiProvider class because we are not using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-
-import './provider/quiz_provider.dart';
-import './provider/time_provider.dart';
 import './screens/ques_type_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //this line is here because of stop unwanted rebuilding widget from MediaQery class
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-  runApp(MultiProvider(
-    providers: [
-      // ChangeNotifierProvider<QuizProvider>.value(value: QuizProvider()),
-      ChangeNotifierProvider<TimeProvider>.value(value: TimeProvider())
-    ],
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
