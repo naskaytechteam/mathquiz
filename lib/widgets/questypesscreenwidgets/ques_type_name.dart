2276553f6ebd06ded7e176d51f6793db4938e281
nikirana22 <nikirana1983@gmail.com>
2022-11-05 21:28:19 +0530

    refactor(ques_type_name):remove blank line

--- lib/widgets/questypesscreenwidgets/ques_type_name.dart
+++ lib/widgets/questypesscreenwidgets/ques_type_name.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-
 import '../../provider/template_factory.dart';
 
 class QuesTypeName extends StatelessWidget {
