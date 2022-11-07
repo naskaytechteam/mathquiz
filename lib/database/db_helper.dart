a3cbf845b568f12126b499fb25ec1d2937fc9d59
nikirana22 <nikirana1983@gmail.com>
2022-11-07 18:17:21 +0530

    refactor(db_helper):make new methods for separating the _getDatabase method logic

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -15,26 +15,43 @@ class DbHelper {
     return _database!;
   }
 
-  Future<Database> _getDatabase() async {
-    String databasePath = await getDatabasesPath();
-    String path = join(databasePath, 'sqlite.db');
-    File file = File(path);
-    bool isFileExist = await file.exists();
-    if (!isFileExist) {
-      await file.create();
+  Future<String> _getPath() async {
+    return join(await getDatabasesPath(), 'sqlite.db');
+  }
+
+  Future<File> _getFile() async {
+    File file = File(await _getPath());
+    if (!await file.exists()) {
+      return file.create();
     }
-    ByteData assetBundle = await rootBundle.load(join('assets', 'sqlite.db'));
-    Uint8List dataList = assetBundle.buffer
-        .asUint8List(assetBundle.offsetInBytes, assetBundle.lengthInBytes);
-    await file.writeAsBytes(dataList);
-    return await openDatabase(path, version: 1);
+    return file;
+  }
+
+  Future<Uint8List> _getAssetsDBFile() async {
+    const assetsDBFileName = 'sqlite.db';
+    ByteData byteData = await rootBundle.load(join('assets', assetsDBFileName));
+
+    return byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+  }
+
+  Future<void> _writeData(File file) async {
+    Uint8List data = await _getAssetsDBFile();
+    await file.writeAsBytes(data);
+  }
+
+  Future<Database> _getDatabase() async {
+    File file = await _getFile();
+    await _writeData(file);
+    return await openDatabase(file.path, version: 1);
   }
 
   Future<List<QuestionTemplate>> readData(TemplateType quesType) async {
     Database db = await database;
     List<Map<String, Object?>> map = await db
         .query('questemp', where: 'quesType=?', whereArgs: [quesType.index]);
-    List<QuestionTemplate> templateList = TemplateParser.questionsTemplateList(map);
+    List<QuestionTemplate> templateList =
+        TemplateParser.questionsTemplateList(map);
     return templateList;
   }
 }
