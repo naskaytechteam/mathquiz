047bb0a3967b6222cffc9ebfe21ec96fbaea7f37
nikirana22 <nikirana1983@gmail.com>
2022-11-18 15:29:39 +0530

    refactor(question_template):added start & end range to provide range of random values.

--- lib/model/question_template.dart
+++ lib/model/question_template.dart
@@ -6,11 +6,19 @@ class QuestionTemplate {
   List<int> values = [];
   num? answer;
   int quesType;
+  int startRange;
+  int endRange;
 
-  QuestionTemplate(this.ques, this.formula, this.valuePlaceholdersCount, this.quesType);
+  QuestionTemplate(this.ques, this.formula, this.valuePlaceholdersCount,
+      this.quesType, this.startRange, this.endRange);
 
   factory QuestionTemplate.fromMap(Map<String, Object?> map) {
-    return QuestionTemplate(map['ques'] as String, map['formula'] as String,
-        map['valuePlaceholder'] as int, map['quesType'] as int);
+    return QuestionTemplate(
+        map['ques'] as String,
+        map['formula'] as String,
+        map['valuePlaceholder'] as int,
+        map['quesType'] as int,
+        map['startRange'] as int,
+        map['endRange'] as int);
   }
 }
