d04420ffb88feea4e92838d66dccbbbf60b4b732
nikirana22 <nikirana1983@gmail.com>
2022-12-02 21:15:59 +0530

    refactor(main):change some colors for new design [WORK IN PROGRESS]

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import '/screens/class_screen.dart';
+import 'package:mathquiz/screens/welcome_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //this line is here because of stop unwanted rebuilding widget from MediaQery class
-  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky,
+      overlays: [SystemUiOverlay.top]);
   runApp(const MyApp());
 }
 
@@ -16,26 +17,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        //background color
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          //background color
+          backgroundColor:const Color.fromRGBO(255, 250, 240, 1),
 
-        backgroundColor: const Color.fromRGBO(54, 58, 102, 1),
-        primaryColor: Colors.blue,
-        colorScheme: const ColorScheme.light(
-            primary: Color.fromRGBO(227, 90, 158, 1),
-            secondary: Color.fromRGBO(190, 106, 228, 1),
-            primaryContainer: Colors.green,
+          //  backgroundColor: const Color.fromRGBO(54, 58, 102, 1),
+          primaryColor: Colors.blue,
+          colorScheme: const ColorScheme.light(
+              primary: Color.fromRGBO(227, 90, 158, 1),
+              secondary: Color.fromRGBO(190, 106, 228, 1),
+              primaryContainer: Colors.green,
 
-            // 56,61,110,1
-            onBackground: Color.fromRGBO(56, 61, 110, 1) //option color
-            ),
-        textTheme: const TextTheme(
-          headline1: TextStyle(fontFamily: primaryFontFamily),
+              // 56,61,110,1
+              onBackground: Color.fromRGBO(56, 61, 110, 1) //option color
+              ),
+          textTheme: const TextTheme(
+              headline1: TextStyle(fontFamily: primaryFontFamily),
+              headline2: TextStyle(fontFamily: 'Nunito')),
         ),
-      ),
-      home: const ClassScreen(),
-    );
+      home: WelcomeScreen(),
+        );
   }
 }
