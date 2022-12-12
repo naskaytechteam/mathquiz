1835f861728addb9b48c6653641f4bd4766d8253
nikirana22 <nikirana1983@gmail.com>
2022-12-12 10:13:36 +0530

    refactor(page3):change class name from page3 to on_boarding_page3

---
+++ lib/widgets/onboardingscreenwidgets/on_boarding_page3.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OnBoardingPage3 extends StatelessWidget {
+  const OnBoardingPage3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double height = size.height;
+    double width = size.width;
+    return Column(
+      children: [
+        SizedBox(height: height * 0.05),
+        SizedBox(
+          height: height * 0.45,
+          width: width,
+          // color: Colors.red,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                bottom: 0,
+                child: SvgPicture.asset(
+                  'assets/images/onboardingbackground3.svg',
+                  // height: 222
+                  height: height * 0.2922,
+                  // width: 253,
+                  width: width * 0.7028,
+                ),
+              ),
+              Positioned(
+                  // bottom: 32.9,
+                  bottom: height * 0.0433,
+                  child: SvgPicture.asset(
+                    'assets/images/onboardingimage3.svg',
+                    // height: 306.25,
+                    height: height * 0.403,
+                    width: width * 0.5456,
+                  ))
+            ],
+          ),
+        ),
+        SizedBox(
+          // height: 52.75,
+          height: height * 0.0694,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+          child: SizedBox(
+            // height: 88,
+            height: height * 0.1158,
+            width: width,
+            //19
+            // color: Colors.greenAccent,
+            child: Text(
+              'Custom Math problems',
+              style: TextStyle(
+                  // fontSize: 32,
+                  fontSize: height * 0.0422,
+                  color: const Color.fromRGBO(52, 73, 94, 1),
+                  fontWeight: FontWeight.w900,
+                  fontFamily: Theme.of(context).textTheme.headline2!.fontFamily),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        SizedBox(
+          // height: 14,
+          height: height * 0.01845,
+        ),
+        SizedBox(
+          // height: 66,
+          height: height * 0.0869,
+          width: width * 0.6,
+          // color: Colors.red,
+          child: Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            style: TextStyle(
+                // fontSize: 16,
+                fontSize: height * 0.0211,
+                fontWeight: FontWeight.w500,
+                fontFamily: Theme.of(context).textTheme.headline2?.fontFamily),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
