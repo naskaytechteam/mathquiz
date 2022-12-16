0288b5b949ccee6b8fb6cd51e591425b45f2e211
nikirana22 <nikirana1983@gmail.com>
2022-12-16 21:19:51 +0530

    refactor(main): add / instead of full package name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:mathquiz/screens/welcome_screen.dart';
+import '/screens/welcome_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,27 +17,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          //background color
-          backgroundColor:const Color.fromRGBO(255, 250, 240, 1),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        //background color
+        backgroundColor: const Color.fromRGBO(255, 250, 240, 1),
 
-          //  backgroundColor: const Color.fromRGBO(54, 58, 102, 1),
-          primaryColor: Colors.blue,
-          colorScheme: const ColorScheme.light(
-              primary: Color.fromRGBO(227, 90, 158, 1),
-              secondary: Color.fromRGBO(190, 106, 228, 1),
-              primaryContainer: Colors.green,
+        //  backgroundColor: const Color.fromRGBO(54, 58, 102, 1),
+        primaryColor: Colors.blue,
+        colorScheme: const ColorScheme.light(
+            primary: Color.fromRGBO(227, 90, 158, 1),
+            secondary: Color.fromRGBO(190, 106, 228, 1),
+            primaryContainer: Colors.green,
 
-              // 56,61,110,1
-              onBackground: Color.fromRGBO(56, 61, 110, 1) //option color
-              ),
-          textTheme: const TextTheme(
-              headline1: TextStyle(fontFamily: primaryFontFamily),
-              headline2: TextStyle(fontFamily: 'Nunito')),
-        ),
-      home: WelcomeScreen(),
-        );
+            // 56,61,110,1
+            onBackground: Color.fromRGBO(56, 61, 110, 1) //option color
+            ),
+        textTheme: const TextTheme(
+            headline1: TextStyle(fontFamily: primaryFontFamily),
+            headline2: TextStyle(fontFamily: 'Nunito')),
+      ),
+      home: const WelcomeScreen(),
+    );
   }
 }
