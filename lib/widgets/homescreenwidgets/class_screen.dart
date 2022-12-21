e711658139503151830802de1d50d7f7826439ea
nikirana22 <nikirana1983@gmail.com>
2022-12-21 20:25:43 +0530

    refactor(class_screen):import profile and userPref classes

--- lib/widgets/homescreenwidgets/class_screen.dart
+++ lib/widgets/homescreenwidgets/class_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import '/screens/profile_screen.dart';
 import 'package:mathquiz/widgets/common_widgets/custom_button.dart';
+import '../../utils/user_preferences.dart';
 import '/widgets/topics.dart';
 import '../homescreenwidgets/class_screen_widgets/resume_your_lession.dart';
 import '/database/db_helper.dart';
