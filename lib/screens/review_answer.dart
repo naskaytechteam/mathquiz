4f103a1ef90c8a49ccb1fd6382099f312d79549a
nikirana22 <nikirana1983@gmail.com>
2022-12-23 19:50:36 +0530

    refactor(review_answer):make all build method local variable as final

--- lib/screens/review_answer.dart
+++ lib/screens/review_answer.dart
@@ -29,11 +29,12 @@ class _ReviewAnswerState extends State<ReviewAnswer> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double height = size.height;
-    double width = size.width;
-    Question question = widget.questions[_quesIndex];
-    bool isFirstQuestion = _quesIndex == 0;
+    final Size size = MediaQuery.of(context).size;
+    final double height = size.height;
+    final double width = size.width;
+    final Question question = widget.questions[_quesIndex];
+    final bool isFirstQuestion = _quesIndex == 0;
+    final bool isLastQuestion=_quesIndex==widget.questions.length-1;
 
     return Scaffold(
       appBar: CustomAppBar(
