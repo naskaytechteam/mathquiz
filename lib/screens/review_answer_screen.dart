36d5b73be8bb351221c3c64b9fd94b82c6699ba7
nikirana22 <nikirana1983@gmail.com>
2022-12-28 15:24:22 +0530

    refactor(review_answer_screen):add SingleChildScrollView

--- lib/screens/review_answer_screen.dart
+++ lib/screens/review_answer_screen.dart
@@ -29,85 +29,87 @@ class ReviewAnswerScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: /*40*/ width * 0.11112),
-        child: Column(
-          children: [
-            _buildGap(height * 0.04737),
-            SizedBox(
-              // height: 22,
-              height: height * 0.029,
-              width: width,
-              child: Text(
-                'Class $classNo - ${templateFactory.currentTemplateType.name}',
-                style: TextStyle(
-                    fontWeight: FontWeight.w900,
-                    // fontSize: 16
-                    fontSize: height * 0.0211),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _buildGap(height * 0.04737),
+              SizedBox(
+                // height: 22,
+                height: height * 0.029,
+                width: width,
+                child: Text(
+                  'Class $classNo - ${templateFactory.currentTemplateType.name}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      // fontSize: 16
+                      fontSize: height * 0.0211),
+                ),
               ),
-            ),
-            _buildGap(height * 0.01975),
-            SizedBox(
-              // height: 50,
-              height: height * 0.0658,
-              width: width,
-              child: Text.rich(
-                const TextSpan(text: 'You answered ', children: [
-                  TextSpan(
-                      text: '9/10 ',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  TextSpan(text: 'questions correctly')
-                ]),
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    // fontSize: 18
-                    fontSize: height * 0.02369),
+              _buildGap(height * 0.01975),
+              SizedBox(
+                // height: 50,
+                height: height * 0.0658,
+                width: width,
+                child: Text.rich(
+                  TextSpan(text: 'You answered ', children: [
+                    TextSpan(
+                        text: '$totalCorrectAnswer/${questions.length} ',
+                        style:const TextStyle(fontWeight: FontWeight.bold)),
+                    const TextSpan(text: 'questions correctly')
+                  ]),
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      // fontSize: 18
+                      fontSize: height * 0.02369),
+                ),
               ),
-            ),
-            _buildGap(height * 0.0395),
-            SizedBox(
-              // height: 520,
-              height: height * 0.68422,
-              width: width,
-              child: ListView.builder(
-                  itemCount: questions.length,
-                  itemBuilder: (_, index) {
-                    int selectedOptionIndex =
-                        questions[index].selectedOptionIndex!;
-                    bool isAnswerCorrect = questions[index].answer ==
-                        questions[index].options[selectedOptionIndex];
-                    String image =
-                        isAnswerCorrect ? 'selected' : 'wrong_answer';
-                    return CustomButton(
-                      height: height * 0.11053,
-                      width: width,
-                      onButtonPressed: () => _onButtonClick(context, index),
-                      backgroundColor: const Color.fromRGBO(236, 240, 241, 1),
-                      shadowColor: isAnswerCorrect
-                          ? const Color.fromRGBO(39, 174, 96, 1)
-                          : const Color.fromRGBO(192, 57, 43, 1),
-                      shadowHeight: height * 0.0093,
-                      margin: EdgeInsets.only(bottom: /*22*/ height * 0.029),
-                      child: ListTile(
-                        trailing: const Icon(Icons.arrow_forward_ios),
-                        title: Text(
-                          'Q.${index + 1}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w900,
-                            // fontSize: 20,
-                            fontSize: height * 0.0264,
+              _buildGap(height * 0.0395),
+              SizedBox(
+                // height: 520,
+                height: height * 0.68422,
+                width: width,
+                child: ListView.builder(
+                    itemCount: questions.length,
+                    itemBuilder: (_, index) {
+                      int selectedOptionIndex =
+                          questions[index].selectedOptionIndex!;
+                      bool isAnswerCorrect = questions[index].answer ==
+                          questions[index].options[selectedOptionIndex];
+                      String image =
+                          isAnswerCorrect ? 'selected' : 'wrong_answer';
+                      return CustomButton(
+                        height: height * 0.11053,
+                        width: width,
+                        onButtonPressed: () => _onButtonClick(context, index),
+                        backgroundColor: const Color.fromRGBO(236, 240, 241, 1),
+                        shadowColor: isAnswerCorrect
+                            ? const Color.fromRGBO(39, 174, 96, 1)
+                            : const Color.fromRGBO(192, 57, 43, 1),
+                        shadowHeight: height * 0.0093,
+                        margin: EdgeInsets.only(bottom: /*22*/ height * 0.029),
+                        child: ListTile(
+                          trailing: const Icon(Icons.arrow_forward_ios),
+                          title: Text(
+                            'Q.${index + 1}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              // fontSize: 20,
+                              fontSize: height * 0.0264,
+                            ),
+                          ),
+                          leading: SvgPicture.asset(
+                            'assets/images/$image.svg',
+                            // height: 21,
+                            height: height * 0.02764,
+                            // width: 22,
+                            width: width * 0.06112,
                           ),
                         ),
-                        leading: SvgPicture.asset(
-                          'assets/images/$image.svg',
-                          // height: 21,
-                          height: height * 0.02764,
-                          // width: 22,
-                          width: width * 0.06112,
-                        ),
-                      ),
-                    );
-                  }),
-            )
-          ],
+                      );
+                    }),
+              )
+            ],
+          ),
         ),
       ),
     );
